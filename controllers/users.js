5fa22605730c5da936add0a096e4d1589589787f
Hasul79 <hatuk2@mail.ru>
2022-09-20 18:16:24 +0400

    add

--- controllers/users.js
+++ controllers/users.js
@@ -6,7 +6,6 @@ export const getUsers = (req, res) => {
   res.send(users);
 }
 
-
 export const createUser = (req, res) => {
     const user = req.body;
 
--- routes/users.js
+++ routes/users.js
@@ -4,16 +4,10 @@ import { createUser , getUsers, getUser, deleteUser, updateUser } from '../contr
 
 const router = express.Router();
 
-
 router.get('/', getUsers);
 
 router.post('/', createUser );
 
-//post միջոցով -տվյալներ ենք ուղարկում դեպի սերվեր/անունը, ազգանունը, տարիքը/
-//հաճախորդից ուղարկվում է սերվեր, որ օգտագործողն ստեղծվի
-
-
-// /users/2 => req.params {id: 2}
 router.get('/:id', getUser );
 
 router.delete('/:id', deleteUser);
