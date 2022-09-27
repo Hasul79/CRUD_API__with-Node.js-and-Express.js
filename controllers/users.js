bdb8a1da5510108adb9de967544fe1124d781df2
Hasul79 <hatuk2@mail.ru>
2022-09-27 10:58:01 +0400

    data.db

--- controllers/users.js
+++ controllers/users.js
@@ -1,21 +1,35 @@
 import { v4 as uuidv4 } from 'uuid';
+import sqlite from 'sqlite3';
+const db = new sqlite.Database('data.db');
 
 let users = [];
  
+db.run("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, firstName TEXT VARCHAR(255) NOT NULL, surName TEXT NOT NULL, age INTEGER NOT NULL)");
+
 export const getUsers = (req, res) => {
   res.send(users);
 }
 
 export const createUser = (req, res) => {
-    const user = req.body;
+  let name = req.body["firstName"]
+  let surname = req.body["surName"]
+  let age = req.body["age"]
+  console.log(name, surname, age)
+  
+  db.run("INSERT INTO users(firstName,surName, age) VALUES(?,?,?)", name, surname, age)
+  res.send(JSON.stringify({message: 'success'}))
+
+
+
+    // const user = req.body;
 
   //  const userId = uuidv4();   
   //  const userWithId = { ...user, id: userId }
   // user.push(userWithId);
 
-     users.push({ ...user, id: uuidv4() });
+    //  users.push({ ...user, id: uuidv4() });
 
-     res.send(`User with the name ${user.firstName} added to the database!`);
+    //  res.send(`User with the name ${user.firstName} added to the database!`);
 }
 
 export const getUser = (req, res) => {
