d700f0bb8fca3bb7f56e6551d228c0e9ff0dbdff
Hasul79 <hatuk2@mail.ru>
2022-10-01 00:29:12 +0400

    add

--- controllers/users.js
+++ controllers/users.js
@@ -1,63 +1,61 @@
-import { v4 as uuidv4 } from 'uuid';
+// import { v4 as uuidv4 } from 'uuid';
 import sqlite from 'sqlite3';
 const db = new sqlite.Database('data.db');
 
 let users = [];
  
-db.run("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, firstName TEXT VARCHAR(255) NOT NULL, surName TEXT NOT NULL, age INTEGER NOT NULL)");
+db.run("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, firstName TEXT VARCHAR(50) NOT NULL, surName TEXT NOT NULL, age INTEGER NOT NULL,  salary INTEGER NOT NULL)");
 
 export const getUsers = (req, res) => {
-  res.send(users);
+ db.all("SELECT * FROM users", function(err, rows){
+   console.log(rows)
+   res.send(rows);
+})
 }
 
 export const createUser = (req, res) => {
   let name = req.body["firstName"]
   let surname = req.body["surName"]
   let age = req.body["age"]
-  console.log(name, surname, age)
+  let salary = req.body["salary"]
+  console.log(name, surname, age, salary)
   
-  db.run("INSERT INTO users(firstName,surName, age) VALUES(?,?,?)", name, surname, age)
+  db.run("INSERT INTO users(firstName,surName, age, salary) VALUES(?,?,?,?)", name, surname, age, salary)
   res.send(JSON.stringify({message: 'success'}))
-
-
-
-    // const user = req.body;
-
-  //  const userId = uuidv4();   
-  //  const userWithId = { ...user, id: userId }
-  // user.push(userWithId);
-
-    //  users.push({ ...user, id: uuidv4() });
-
-    //  res.send(`User with the name ${user.firstName} added to the database!`);
-}
+};
 
 export const getUser = (req, res) => {
-  const { id } = req.params;
+  const id = req.params.id
+  db.all("SELECT * FROM users WHERE id = ?",[id],  function(err, rows){
+    console.log(rows);
+    res.send(rows);
+ }
+ )};
+    
+ export const updateUser = (req, res) => {
+  const id = req.params.id
+  let firstName = req.body["firstName"];
+  let surname = req.body["surName"];
+  let age = req.body["age"];
+  let salary = req.body["salary"];
 
-  const foundUser = users.find((user) => user.id === id);
-    res.send(foundUser);
+  let sqlite =`UPDATE users SET firstName = ?, surName = ?, age = ? , salary = ?  WHERE id = ${id}`
+  db.run(sqlite,firstName,surname,age, salary)
+  res.send(`User with the ${id} has been updated`)
 }
- 
-export const deleteUser =(req, res) => {
-  const { id } = req.params;
+   
+  export const deleteUser =(req, res) => {
+    const id = req.params.id
   
-  users = users.filter((user) => user.id !== id);
-  res.send(`User with the id ${id} deleted from the database`);
-}
+    db.run("DELETE FROM  users WHERE id=?",[id], function(err){
+    db.all("SELECT * FROM users WHERE id = ?",[id],  function(err, rows){
+      console.log(rows);
+      res.send(rows);
+    })
+  })
+  };
+  
+    
+    
 
 
-export const updateUser = (req, res) => {
-  const { id } = req.params;
-  const { firstName, lastName, age } = req.body;
-
-  const user = users.find((user) => user.id === id);
-
-      if (firstName) user.firstName = firstName;
-      
-      if (lastName) user.lastName = lastName;
-      
-      if (age) user.age = age;
-      
-  res.send(`User with the id ${id} has been updated`);
-}
